12f701f913dadd53ca673115a79b061d44995c8b
ilinnixon <149894196+ilinnixon@users.noreply.github.com>
2025-03-25 22:56:55 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
-import FoodBudget from "./components/FoodBudget";
+import BudgetChecker from "./components/BudgetChecker";
 import Login from "./components/Login";
+import SignUp from "./components/SignUp"; // Import SignUp
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/food-check" element={<FoodBudget />} />
+        <Route path="/BudgetChecker" element={<BudgetChecker />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} /> {/* Added Sign Up Route */}
       </Routes>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
